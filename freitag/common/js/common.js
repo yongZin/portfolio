d2f9a5ec5da75e2109e0bda83a37cbbdb5df06b4
yongZin-MAC <>
2024-07-25 17:02:08 +0900

    style: Add product upload portfolio

--- freitag/common/js/common.js
+++
@@ -1,7 +0,0 @@
-const container = document.querySelector('html');
-const topBtn = document.querySelector('.footer__top--btn');
-
-topBtn.addEventListener('click', function(e){
-    e.preventDefault();
-    window.scrollTo({top: 0, behavior: 'smooth'})
-});
