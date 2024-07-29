c31ae2afb3633bdc6b9122fe4b7f7968b13742ff
yongZin-MAC <>
2024-07-29 14:59:18 +0900

    chore: Modify API index.js

--- api/index.js
+++ api/index.js
@@ -16,21 +16,3 @@ app.listen(MY_APP_PORT, () => {
 });
 
 module.exports = app;
-
-// mongoose.connect( // MongoBD 클러스터 연결 (admin / Tdm6NtGLhGB9bRgp)
-// 	process.env.MONGO_URI
-// )
-// .then(() => {
-// 	console.log("mongoDB Conneected.");
-	
-// 	app.use(express.static("build"));
-// 	app.get("/", (req, res) => {
-// 		res.sendFile(__dirname + "/build/index.html");
-// 	})
-// 	app.use(express.json()); //req.body를 json형식으로 변경(미들웨어 설정)
-// 	app.use("/ranks", rankRouter);
-// 	app.listen(MY_APP_PORT, () => {
-// 		console.log(`Server port ${MY_APP_PORT}`)
-// 	});
-// })
-// .catch((err) => console.log(err))
